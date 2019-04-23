2cf336337758ee2405d08e5eb58f7f9430e12277
Rolando Fernandez <rolando@ebi.ac.uk>
2019-04-23 16:24:37 +0100

    added tests

---
+++ test/service/test_spreadsheet_storage_service.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+
+
+from unittest import TestCase
+from broker.service.spreadsheet_storage.spreadsheet_storage_service import SpreadsheetStorageService
+from broker.service.spreadsheet_storage.spreadsheet_storage_exceptions import SubmissionSpreadsheetDoesntExist, SubmissionSpreadsheetAlreadyExists
+
+
+TEST_STORAGE_DIR = "test_storage_dir"
+
+
+class SpreadsheetStorageServiceTest(TestCase):
+
+    def setUp(self):
+        os.mkdir(TEST_STORAGE_DIR)
+
+    def test_store_spreadsheet(self):
+        test_storage_dir = "test_storage_dir"
+        mock_submission_uuid = "mock-uuid"
+        mock_spreadsheet_name = "mock_spreadsheet.xls"
+        mock_spreadsheet_blob = bytes.fromhex('6d6f636b64617461')
+        spreadsheet_storage_service = SpreadsheetStorageService(test_storage_dir)
+
+        try:
+            spreadsheet_storage_service.store(mock_submission_uuid, mock_spreadsheet_name, mock_spreadsheet_blob)
+            assert True
+        except Exception as e:
+            assert False
+
+    def test_retrieve_spreadsheet(self):
+        test_storage_dir = "test_storage_dir"
+        mock_submission_uuid = "mock-uuid"
+        mock_spreadsheet_name = "mock_spreadsheet.xls"
+        mock_spreadsheet_blob = bytes.fromhex('6d6f636b64617461')
+        spreadsheet_storage_service = SpreadsheetStorageService(test_storage_dir)
+
+        try:
+            spreadsheet_storage_service.store(mock_submission_uuid, mock_spreadsheet_name, mock_spreadsheet_blob)
+            spreadsheet = spreadsheet_storage_service.retrieve(mock_submission_uuid)
+            assert spreadsheet["name"] == mock_spreadsheet_name
+            assert spreadsheet["blob"] == mock_spreadsheet_blob
+        except Exception as e:
+            assert False
+
+    def tearDown(self):
+        shutil.rmtree(TEST_STORAGE_DIR)
