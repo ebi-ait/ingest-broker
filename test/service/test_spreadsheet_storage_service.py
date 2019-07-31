b4232656ea33f8bc2900f408758b837f661f1336
Rodrey Goite <rodrey@ebi.ac.uk>
2019-07-31 14:44:33 +0100

    Restructured test_store_spreadsheet; added assertions.

--- test/service/test_spreadsheet_storage_service.py
+++ test/service/test_spreadsheet_storage_service.py
@@ -1,11 +1,9 @@
 import os
 import shutil
-
-
+from tempfile import TemporaryDirectory
 from unittest import TestCase
-from broker.service.spreadsheet_storage.spreadsheet_storage_service import SpreadsheetStorageService
-from broker.service.spreadsheet_storage.spreadsheet_storage_exceptions import SubmissionSpreadsheetDoesntExist, SubmissionSpreadsheetAlreadyExists
 
+from broker.service.spreadsheet_storage.spreadsheet_storage_service import SpreadsheetStorageService
 
 TEST_STORAGE_DIR = "test_storage_dir"
 
@@ -16,17 +14,26 @@ class SpreadsheetStorageServiceTest(TestCase):
         os.mkdir(TEST_STORAGE_DIR)
 
     def test_store_spreadsheet(self):
-        test_storage_dir = "test_storage_dir"
-        mock_submission_uuid = "mock-uuid"
-        mock_spreadsheet_name = "mock_spreadsheet.xls"
-        mock_spreadsheet_blob = bytes.fromhex('6d6f636b64617461')
-        spreadsheet_storage_service = SpreadsheetStorageService(test_storage_dir)
+        with TemporaryDirectory() as storage_root:
+            # given:
+            spreadsheet_storage_service = SpreadsheetStorageService(storage_root)
 
-        try:
-            spreadsheet_storage_service.store(mock_submission_uuid, mock_spreadsheet_name, mock_spreadsheet_blob)
-            assert True
-        except Exception as e:
-            assert False
+            # and:
+            submission_uuid = "78451f36-c782-4d2d-8491-47e06ddb860f"
+            file_name = "test_spreadsheet.xls"
+            spreadsheet_data = bytes.fromhex('6d6f636b64617461')
+
+            # when:
+            spreadsheet_storage_service.store(submission_uuid, file_name, spreadsheet_data)
+
+            # then:
+            spreadsheet_directory = f'{storage_root}/{submission_uuid}'
+            self.assertTrue('Spreadsheet directory does not exist.',
+                            os.path.exists(spreadsheet_directory))
+
+            # and:
+            spreadsheet_files = os.listdir(spreadsheet_directory)
+            self.assertTrue(file_name in spreadsheet_files)
 
     def test_retrieve_spreadsheet(self):
         test_storage_dir = "test_storage_dir"
@@ -36,7 +43,8 @@ class SpreadsheetStorageServiceTest(TestCase):
         spreadsheet_storage_service = SpreadsheetStorageService(test_storage_dir)
 
         try:
-            spreadsheet_storage_service.store(mock_submission_uuid, mock_spreadsheet_name, mock_spreadsheet_blob)
+            spreadsheet_storage_service.store(mock_submission_uuid, mock_spreadsheet_name,
+                                              mock_spreadsheet_blob)
             spreadsheet = spreadsheet_storage_service.retrieve(mock_submission_uuid)
             assert spreadsheet["name"] == mock_spreadsheet_name
             assert spreadsheet["blob"] == mock_spreadsheet_blob
