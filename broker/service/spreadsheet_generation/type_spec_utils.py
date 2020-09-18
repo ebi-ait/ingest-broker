c92a89f3f17e6f63d0a85cf069be1c3be3b7e962
Rodrey Goite <rodrey@ebi.ac.uk>
2020-09-18 10:58:42 +0100

    Added check if link_spec is defined.

--- broker/service/spreadsheet_generation/type_spec_utils.py
+++ broker/service/spreadsheet_generation/type_spec_utils.py
@@ -14,13 +14,14 @@ def sort(specs):
             data = Data(spec, data.back_links)
             adjacency[spec.schema_name] = data
 
-        if len(spec.link_spec.link_entities) == 0:
-            stack.insert(0, spec)  # keep the original ordering
-        else:
-            for link_entity in spec.link_spec.link_entities:
-                if link_entity not in adjacency.keys():
-                    adjacency[link_entity] = Data(None, set())
-                adjacency[link_entity].back_links.add(spec.schema_name)
+        if spec.link_spec is not None:
+            if len(spec.link_spec.link_entities) == 0:
+                stack.insert(0, spec)  # keep the original ordering
+            else:
+                for link_entity in spec.link_spec.link_entities:
+                    if link_entity not in adjacency.keys():
+                        adjacency[link_entity] = Data(None, set())
+                    adjacency[link_entity].back_links.add(spec.schema_name)
 
     if len(stack) > 0:
         specs.clear()
