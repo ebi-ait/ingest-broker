b5b566674d9ad1d908e0cb2b56ade630f7d1902f
Rodrey Goite <rodrey@ebi.ac.uk>
2020-09-18 15:21:35 +0100

    Modified sort utility to use breadth-first ordering.

--- test/service/spreadsheet_generation/test_type_spec_sorter.py
+++ test/service/spreadsheet_generation/test_type_spec_sorter.py
@@ -4,6 +4,11 @@ from broker.service.spreadsheet_generation import type_spec_utils
 from broker.service.spreadsheet_generation.spreadsheet_generator import TypeSpec, LinkSpec
 
 
+def _type_spec(name, entity_links=None) -> TypeSpec:
+    return TypeSpec(schema_name=name,
+                    link_spec=LinkSpec(link_entities=entity_links if entity_links is not None else []))
+
+
 class TypeSpecSorterTest(TestCase):
 
     def test_no_links_first(self):
@@ -20,3 +25,22 @@ class TypeSpecSorterTest(TestCase):
         # then:
         self.assertEqual(3, len(spec))
         self.assertListEqual(spec, [child_1, parent, child_2])
+
+    def test_breadth_first_ordering(self):
+        # given:
+        donor = _type_spec('donor')
+        specimen = _type_spec('specimen', [donor.schema_name])
+        organoid = _type_spec('organoid', [specimen.schema_name])
+        cell_line = _type_spec('cell_line', [specimen.schema_name])
+        cell_suspension = _type_spec('cell_suspension', [organoid.schema_name, cell_line.schema_name])
+        imaged_specimen = _type_spec('imaged_specimen', [organoid.schema_name])
+        sequence_file = _type_spec('sequence_file', [cell_suspension.schema_name])
+
+        # when:
+        spec = [specimen, sequence_file, imaged_specimen, donor, organoid, cell_line, cell_suspension]
+        type_spec_utils.sort(spec)
+
+        # then:
+        self.assertEqual(7, len(spec))
+        self.assertListEqual(spec, [donor, specimen, organoid, cell_line, imaged_specimen, cell_suspension,
+                                    sequence_file])
