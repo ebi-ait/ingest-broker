4083c964363e28a34ce16d12de49c98baf58c593
Aclan Alegria <aaclan@ebi.ac.uk>
2020-10-19 14:23:23 +0100

    Added some typing in schema service methods

--- broker/service/schema_service.py
+++ broker/service/schema_service.py
@@ -11,11 +11,11 @@ class SchemaService(object):
         json_ref_obj = jsonref.loads(json.dumps(json_schema), loader=self.json_loader)
         return json_ref_obj
 
-    def get_json_schema(self, schema_url):
+    def get_json_schema(self, schema_url: str) -> dict:
         json_schema = self.json_loader(schema_url)
         return json_schema
 
-    def get_dereferenced_schema(self, schema_url):
+    def get_dereferenced_schema(self, schema_url: str) -> dict:
         json_schema = self.get_json_schema(schema_url)
         deref_schema_str = str(self._dereference_schema(json_schema))
         deref_schema_json = ast.literal_eval(deref_schema_str)
