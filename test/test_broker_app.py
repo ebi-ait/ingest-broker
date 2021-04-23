a1703a322fe93b6af3d2e61144e1caa87a14d6e3
Jacob Windsor <me@jcbwndsr.com>
2021-04-23 12:12:21 +0200

    Use request context for tests

--- test/test_broker_app.py
+++ test/test_broker_app.py
@@ -3,6 +3,7 @@ from unittest.mock import patch, Mock
 
 from broker.service.spreadsheet_upload_service import SpreadsheetUploadError
 from broker_app import app as _app, setup
+import flask, broker_app
 
 
 class BrokerAppTest(TestCase):
@@ -10,8 +11,8 @@ class BrokerAppTest(TestCase):
     @patch('broker_app.IngestApi')
     def setUp(self, mock_ingest, mock_spreadsheet_generator):
         setup()
-        _app.testing = True
         self.app = _app.test_client()
+        self.request_context = _app.test_request_context()
 
     @patch('broker_app.os.environ')
     @patch('broker_app.IngestApi')
@@ -34,116 +35,122 @@ class BrokerAppTest(TestCase):
         # then
         self.assertEqual(response.status_code, 302)
 
-    @patch('broker_app.request')
     @patch('broker_app.SpreadsheetUploadService.async_upload')
     @patch('broker_app.IngestApi')
-    def test_upload_success(self, mock_ingest, mock_async_upload, mock_request):
-        # given
-        mock_async_upload.return_value = {'uuid': {'uuid': 'uuid'}, '_links': {'self': {'href': 'url/9'}}}
-        mock_request.files = {'file': 'content'.encode()}
-        mock_request.form = {}
-        mock_headers = Mock('headers')
-        mock_headers.get = Mock(return_value='token')
-        mock_request.headers = mock_headers
+    def test_upload_success(self, mock_ingest, mock_async_upload):
+        with self.request_context:
+            with patch.object(broker_app, "request") as mock_request:
+                mock_async_upload.return_value = {'uuid': {'uuid': 'uuid'}, '_links': {'self': {'href': 'url/9'}}}
+                mock_request.files = {'file': 'content'.encode()}
+                mock_request.form = {}
+                mock_headers = Mock('headers')
+                mock_headers.get = Mock(return_value='token')
+                mock_request.headers = mock_headers
 
-        # when
-        response = self.app.post('/api_upload')
+                # when
+                response = self.app.post('/api_upload')
 
-        # then
-        self.assertEqual(response.status_code, 201)
-        self.assertRegex(str(response.data), 'url/9')
-        mock_async_upload.assert_called_with('token', 'content'.encode(), False, None)
+                # then
+                self.assertEqual(response.status_code, 201)
+                self.assertRegex(str(response.data), 'url/9')
+                mock_async_upload.assert_called_with('token', 'content'.encode(), False, None)
 
-    @patch('broker_app.request')
     @patch('broker_app.SpreadsheetUploadService.async_upload')
     @patch('broker_app.IngestApi')
-    def test_upload_error(self, mock_ingest, mock_async_upload, mock_request):
-        # given
-        mock_async_upload.side_effect = SpreadsheetUploadError(500, 'message', 'details')
-        mock_request.files = {'file': 'content'.encode()}
-        mock_request.form = {}
-        mock_headers = Mock('headers')
-        mock_headers.get = Mock(return_value='token')
-        mock_request.headers = mock_headers
+    def test_upload_error(self, mock_ingest, mock_async_upload):
+        with self.request_context:
+            with patch.object(broker_app, "request") as mock_request:
+                # given
+                mock_async_upload.side_effect = SpreadsheetUploadError(500, 'message', 'details')
+                mock_request.files = {'file': 'content'.encode()}
+                mock_request.form = {}
+                mock_headers = Mock('headers')
+                mock_headers.get = Mock(return_value='token')
+                mock_request.headers = mock_headers
 
-        # when
-        response = self.app.post('/api_upload')
+                # when
+                response = self.app.post('/api_upload')
 
-        # then
-        self.assertEqual(response.status_code, 500)
-        self.assertRegex(str(response.data), 'message')
-        mock_async_upload.assert_called_with('token', 'content'.encode(), False, None)
+                # then
+                self.assertEqual(response.status_code, 500)
+                self.assertRegex(str(response.data), 'message')
+                mock_async_upload.assert_called_with('token', 'content'.encode(), False, None)
 
-    @patch('broker_app.request')
     @patch('broker_app.IngestApi')
-    def test_upload_unauthorized(self, mock_ingest, mock_request):
-        # given
-        mock_request.files = {'file': 'content'.encode()}
-        mock_request.form = {}
-        mock_headers = Mock('headers')
-        mock_headers.get = Mock(return_value=None)
-        mock_request.headers = mock_headers
+    def test_upload_unauthorized(self, mock_ingest):
+        with self.request_context:
+            with patch.object(broker_app, "request") as mock_request:
+                # given
+                mock_request = patch.object(flask, "request")
+                mock_request.files = {'file': 'content'.encode()}
+                mock_request.form = {}
+                mock_headers = Mock('headers')
+                mock_headers.get = Mock(return_value=None)
+                mock_request.headers = mock_headers
 
-        # when
-        response = self.app.post('/api_upload')
+                # when
+                response = self.app.post('/api_upload')
 
-        # then
-        self.assertEqual(response.status_code, 401)
-        self.assertRegex(str(response.data), 'authentication')
+                # then
+                self.assertEqual(response.status_code, 401)
+                self.assertRegex(str(response.data), 'authentication')
 
-    @patch('broker_app.request')
     @patch('broker_app.SpreadsheetUploadService.async_upload')
     @patch('broker_app.IngestApi')
-    def test_upload_update_success(self, mock_ingest, mock_async_upload, mock_request):
-        # given
-        mock_async_upload.return_value = {'uuid': {'uuid': 'uuid'}, '_links': {'self': {'href': 'url/9'}}}
-        mock_request.files = {'file': 'content'.encode()}
-        mock_request.form = {}
-        mock_headers = Mock('headers')
-        mock_headers.get = Mock(return_value='token')
-        mock_request.headers = mock_headers
+    def test_upload_update_success(self, mock_ingest, mock_async_upload):
+        with self.request_context:
+            with patch.object(broker_app, "request") as mock_request:
+                # given
+                mock_async_upload.return_value = {'uuid': {'uuid': 'uuid'}, '_links': {'self': {'href': 'url/9'}}}
+                mock_request.files = {'file': 'content'.encode()}
+                mock_request.form = {}
+                mock_headers = Mock('headers')
+                mock_headers.get = Mock(return_value='token')
+                mock_request.headers = mock_headers
 
-        # when
-        response = self.app.post('/api_upload_update')
+                # when
+                response = self.app.post('/api_upload_update')
 
-        # then
-        self.assertEqual(response.status_code, 201)
-        self.assertRegex(str(response.data), 'url/9')
-        mock_async_upload.assert_called_with('token', 'content'.encode(), True, None)
+                # then
+                self.assertEqual(response.status_code, 201)
+                self.assertRegex(str(response.data), 'url/9')
+                mock_async_upload.assert_called_with('token', 'content'.encode(), True, None)
 
-    @patch('broker_app.request')
     @patch('broker_app.SpreadsheetUploadService.async_upload')
     @patch('broker_app.IngestApi')
-    def test_upload_update_error(self, mock_ingest, mock_async_upload, mock_request):
-        # given
-        mock_async_upload.side_effect = SpreadsheetUploadError(500, 'message', 'details')
-        mock_request.files = {'file': 'content'.encode()}
-        mock_request.form = {}
-        mock_headers = Mock('headers')
-        mock_headers.get = Mock(return_value='token')
-        mock_request.headers = mock_headers
+    def test_upload_update_error(self, mock_ingest, mock_async_upload):
+        with self.request_context:
+            with patch.object(broker_app, "request") as mock_request:
+                # given
+                mock_async_upload.side_effect = SpreadsheetUploadError(500, 'message', 'details')
+                mock_request.files = {'file': 'content'.encode()}
+                mock_request.form = {}
+                mock_headers = Mock('headers')
+                mock_headers.get = Mock(return_value='token')
+                mock_request.headers = mock_headers
 
-        # when
-        response = self.app.post('/api_upload_update')
+                # when
+                response = self.app.post('/api_upload_update')
 
-        # then
-        self.assertEqual(response.status_code, 500)
-        self.assertRegex(str(response.data), 'message')
-        mock_async_upload.assert_called_with('token', 'content'.encode(), True, None)
+                # then
+                self.assertEqual(response.status_code, 500)
+                self.assertRegex(str(response.data), 'message')
+                mock_async_upload.assert_called_with('token', 'content'.encode(), True, None)
 
-    @patch('broker_app.request')
     @patch('broker_app.IngestApi')
-    def test_upload_update_unauthorized(self, mock_ingest, mock_request):
-        # given
-        mock_request.files = {'file': 'content'.encode()}
-        mock_request.form = {}
-        mock_headers = Mock('headers')
-        mock_headers.get = Mock(return_value=None)
-        mock_request.headers = mock_headers
+    def test_upload_update_unauthorized(self, mock_ingest):
+        with self.request_context:
+            with patch.object(broker_app, "request") as mock_request:
+                # given
+                mock_request.files = {'file': 'content'.encode()}
+                mock_request.form = {}
+                mock_headers = Mock('headers')
+                mock_headers.get = Mock(return_value=None)
+                mock_request.headers = mock_headers
 
-        # when
-        response = self.app.post('/api_upload_update')
+                # when
+                response = self.app.post('/api_upload_update')
 
-        # then
-        self.assertEqual(response.status_code, 401)
-        self.assertRegex(str(response.data), 'authentication')
+                # then
+                self.assertEqual(response.status_code, 401)
+                self.assertRegex(str(response.data), 'authentication')
