ee6a0a115c98c2813bc8f67e2ba3990d7fa7a00e
Alegria Aclan <aaclan@ebi.ac.uk>
2021-07-07 21:20:57 +0100

    Refactored async_upload for readability

--- broker/service/spreadsheet_upload_service.py
+++ broker/service/spreadsheet_upload_service.py
@@ -19,36 +19,44 @@ class SpreadsheetUploadService:
         self.importer = importer
 
     def async_upload(self, token, request_file, is_update, project_uuid=None, submission_uuid=None):
-        if token is None:
-            raise SpreadsheetUploadError(401, "An authentication token must be supplied when uploading a spreadsheet")
-
-        self.ingest_api.set_token(token)
-
-        if submission_uuid:
-            submission_resource = self.ingest_api.get_submission_by_uuid(submission_uuid)
-        else:
-            submission_resource = self.ingest_api.create_submission(update_submission=is_update)
-
-        filename = secure_filename(request_file.filename)
+        self._assert_token(token)
+        submission_resource = self._create_or_get_submission(submission_uuid)
 
         submission_uuid = submission_resource["uuid"]["uuid"]
         submission_url = submission_resource["_links"]["self"]["href"]
-        submission_directory = self.storage_service.get_submission_dir(submission_uuid)
+        filename = secure_filename(request_file.filename)
 
         if is_update:
-            timestamp = time.strftime("%Y%m%d-%H%M%S")
-            filename_with_timestamp = f'{timestamp}_{filename}'
-            # TODO This spreadsheet containing the updates is not downloadable anywhere yet
-            path = self.storage_service.store_binary_file(submission_directory, filename_with_timestamp, request_file.read())
-
+            path = self._store_spreadsheet_updates(filename, request_file, submission_uuid)
             thread = threading.Thread(target=self.upload_updates, args=(submission_url, path))
         else:
             path = self.storage_service.store_submission_spreadsheet(submission_uuid, filename, request_file.read())
-            thread = threading.Thread(target=self.upload,
-                                      args=(submission_url, path, project_uuid))
+            thread = threading.Thread(target=self.upload, args=(submission_url, path, project_uuid))
+
         thread.start()
 
         return submission_resource
+
+    def _store_spreadsheet_updates(self, filename, request_file, submission_uuid):
+        submission_directory = self.storage_service.get_submission_dir(submission_uuid)
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        filename_with_timestamp = f'{timestamp}_{filename}'
+        # TODO This spreadsheet containing the updates is not downloadable anywhere yet
+        path = self.storage_service.store_binary_file(submission_directory, filename_with_timestamp,
+                                                      request_file.read())
+        return path
+
+    def _create_or_get_submission(self, submission_uuid):
+        if submission_uuid:
+            submission_resource = self.ingest_api.get_submission_by_uuid(submission_uuid)
+        else:
+            submission_resource = self.ingest_api.create_submission()
+        return submission_resource
+
+    def _assert_token(self, token):
+        if token is None:
+            raise SpreadsheetUploadError(401, "An authentication token must be supplied when uploading a spreadsheet")
+        self.ingest_api.set_token(token)
 
     def upload(self, submission_url, path, project_uuid=None):
         _LOGGER.info('Spreadsheet started!')
