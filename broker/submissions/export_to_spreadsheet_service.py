f51a2c014526dad92e6a6f5d2c8f8381876dc693
Alegria Aclan <aaclan@ebi.ac.uk>
2021-08-10 09:45:33 +0800

    Unset any token set to ingest api object to avoid passing any stale token

--- broker/submissions/export_to_spreadsheet_service.py
+++ broker/submissions/export_to_spreadsheet_service.py
@@ -6,11 +6,12 @@ from ingest.downloader.downloader import XlsDownloader
 class ExportToSpreadsheetService:
 
     def __init__(self, ingest_api: IngestApi = None):
-        self.ingestapi = IngestApi() if not ingest_api else ingest_api
-        self.data_collector = DataCollector(self.ingestapi)
+        self.ingest_api = IngestApi() if not ingest_api else ingest_api
+        self.data_collector = DataCollector(self.ingest_api)
         self.downloader = XlsDownloader()
 
     def export(self, submission_uuid: str):
+        self.ingest_api.unset_token()
         entity_list = self.data_collector.collect_data_by_submission_uuid(submission_uuid)
         flattened_json = self.downloader.convert_json(entity_list)
         workbook = self.downloader.create_workbook(flattened_json)
