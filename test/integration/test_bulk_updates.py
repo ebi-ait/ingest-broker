0cf2e1a7279d221e9ed3e0e69c94ce023e5a6ae9
Karoly Erdos <karoly@ebi.ac.uk>
2021-08-16 15:59:34 +0100

    export -> modify sheet -> import works fine
    TODO: need to add import as the 1st step

--- test/integration/test_bulk_updates.py
+++ test/integration/test_bulk_updates.py
@@ -1,4 +1,5 @@
 import time
+from pathlib import Path
 from unittest import TestCase
 
 import requests
@@ -18,9 +19,12 @@ class BulkUpdateTest(TestCase):
     def setUp(self) -> None:
         # TODO add configuration to get ingest url from env variables
         # TODO currently it is hardcode for dev API
+        # TODO I am going to move this test to the integration test package where I don't need to hardcode a JWT Token
+        # TODO then all the above is going to be solved
         self.ingest_url = "https://api.ingest.dev.archive.data.humancellatlas.org"
+        jwt_token = '<TOKEN>'
         self.ingest_api = IngestApi(self.ingest_url)
-        # self.ingest_api = IngestApi()
+        self.ingest_api.set_token(jwt_token)
 
         self.export_service = ExportToSpreadsheetService(self.ingest_api)
 
@@ -28,56 +32,65 @@ class BulkUpdateTest(TestCase):
         self.submission_url = None
         self.data_collector = DataCollector(self.ingest_api)
         self.downloader = XlsDownloader()
+        self.path_to_spreadsheet = None
+        self.modified_project_id = None
+        self.modified_biomaterial_id = None
+        self.original_project_from_api = None
+        self.original_biomaterial_from_api = None
 
-    def test_download_spreadsheet(self):
-        # download the test spreadsheet for the new submission
+    def tearDown(self) -> None:
+        self.remove_temp_worksheet()
 
-        # create a submission, track the submission url
-        self.submission_url = 'http://localhost:8080/submissionEnvelopes/60f570c9a8a292649e814171'
+        self.revert_original_content('projects', self.modified_project_id, self.original_project_from_api)
+        self.revert_original_content('biomaterials', self.modified_biomaterial_id, self.original_biomaterial_from_api)
 
-        # download a spreadsheet
-        entity_list = self.data_collector.collect_data_by_submission_uuid('98ec9cbb-e76c-4f8f-9b97-98fce7b151cb')
-        flattened_json = self.downloader.convert_json(entity_list)
-        workbook = self.downloader.create_workbook(flattened_json)
-        workbook.save('downloaded-spreadsheet-original.xlsx')
+    def remove_temp_worksheet(self):
+        sheet_path = Path(self.path_to_spreadsheet)
+        sheet_path.unlink()
 
-        # assert spreadsheet contains data from submission
+    def revert_original_content(self, entity_type, entity_id, original_content):
+        self.ingest_api.patch(self.ingest_url + f'/{entity_type}/' + entity_id, original_content)
 
-    def test_import_modify_and_export_spreadsheet_flow(self):
-        # TODO: add JWT token for auth - without it the import won't work
+    def test_export__modify__import_spreadsheet_flow(self):
         # download a spreadsheet given a submission uuid /<submission_uuid>/spreadsheet
         submission_uuid = 'e9e923d8-30b5-4184-87c5-fae40ce01ac9'
         submission = self.ingest_api.get_submission_by_uuid(submission_uuid)
         submission_id = submission['_links']['self']['href'].split('/')[-1]
-        path_to_spreadsheet, spreadsheet = self.__export_spreadsheet(submission_uuid)
+        self.path_to_spreadsheet, spreadsheet = self.__export_spreadsheet(submission_uuid)
 
         # validate same cell values and then modify it then check it again that it has been modified
         project_sheet: Worksheet = spreadsheet.get_sheet_by_name('Project')
-        updated_project_title, modified_project_id = self.__update_project_title(project_sheet)
+        updated_project_title, self.modified_project_id = self.__update_project_title(project_sheet)
 
         specimen_sheet: Worksheet = spreadsheet.get_sheet_by_name('Specimen from organism')
-        updated_biomaterial_name, modified_biomaterial_id = self.__update_biomaterial_name(specimen_sheet)
+        updated_biomaterial_name, self.modified_biomaterial_id = self.__update_biomaterial_name(specimen_sheet)
 
-        self.__save_modified_spreadsheet(path_to_spreadsheet, spreadsheet)
+        self.__save_modified_spreadsheet(self.path_to_spreadsheet, spreadsheet)
+
+        # get the original metadata
+        self.original_project_from_api = self.__get_entity_json_by_type_and_id('projects', self.modified_project_id)
+        self.original_biomaterial_from_api = self.__get_entity_json_by_type_and_id('biomaterials', self.modified_biomaterial_id)
 
         # import the modified spreadsheet
         submission_url = self.ingest_url + '/submissionEnvelopes/' + submission_id
-        self.importer.import_file(path_to_spreadsheet, submission_url, is_update=True)
+        self.importer.import_file(self.path_to_spreadsheet, submission_url, is_update=True)
 
         # get the modified metadata
-        updated_project_from_api = requests.get(self.ingest_url + '/projects/' + modified_project_id).json()
-        updated_biomaterial_from_api = requests.get(self.ingest_url + '/biomaterials/' + modified_biomaterial_id).json()
+        updated_project_from_api = self.__get_entity_json_by_type_and_id('projects', self.modified_project_id)
+        updated_biomaterial_from_api = self.__get_entity_json_by_type_and_id('biomaterials', self.modified_biomaterial_id)
 
         # assert that the update has been applied on the metadata
         self.assertEqual(
-            updated_project_from_api['content']['project_core']['project_title'], updated_project_title)
+            updated_project_title, updated_project_from_api['content']['project_core']['project_title'])
         self.assertEqual(
-            updated_biomaterial_from_api['content']['biomaterial_core']['biomaterial_name'], updated_biomaterial_name)
+            updated_biomaterial_name, updated_biomaterial_from_api['content']['biomaterial_core']['biomaterial_name'])
+
+    def __get_entity_json_by_type_and_id(self, entity_type, entity_id):
+        return requests.get(self.ingest_url + f'/{entity_type}/' + entity_id).json()
 
     def __export_spreadsheet(self, submission_uuid):
         spreadsheet = self.export_service.export(submission_uuid)
         timestamp = time.strftime("%Y%m%d-%H%M%S")
-        # temp_file = tempfile.NamedTemporaryFile()
         filename = f'{submission_uuid}_{timestamp}.xlsx'
         path_to_spreadsheet = 'temp_resources/' + filename
         spreadsheet.save(path_to_spreadsheet)
@@ -86,7 +99,7 @@ class BulkUpdateTest(TestCase):
 
     def __update_project_title(self, project_sheet):
         orig_project_title = project_sheet.cell(6, 7).value
-        updated_project_title = orig_project_title + ' UPDATED'
+        updated_project_title = orig_project_title + ' UPDATED 999'
         project_sheet.cell(6, 7, updated_project_title)
 
         self.assertEqual(updated_project_title, project_sheet.cell(6, 7).value)
@@ -96,11 +109,11 @@ class BulkUpdateTest(TestCase):
         return updated_project_title, modified_project_id
 
     def __update_biomaterial_name(self, specimen_sheet):
-        orig_biomaterial_name = specimen_sheet.cell(6, 3).value
-        updated_biomaterial_name = orig_biomaterial_name + ' UPDATED'
-        specimen_sheet.cell(6, 3, updated_biomaterial_name)
+        orig_biomaterial_name = specimen_sheet.cell(6, 4).value
+        updated_biomaterial_name = orig_biomaterial_name + ' UPDATED 123'
+        specimen_sheet.cell(6, 4, updated_biomaterial_name)
 
-        self.assertEqual(updated_biomaterial_name, specimen_sheet.cell(6, 3).value)
+        self.assertEqual(updated_biomaterial_name, specimen_sheet.cell(6, 4).value)
 
         modified_biomaterial_id = self.__get_entity_id(specimen_sheet, 'biomaterials')
 
