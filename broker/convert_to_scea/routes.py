430f6402283955563ffa28c7bdcc6b25fb9ee5ba
Ami Day <ami@ebi.ac.uk>
2022-03-23 16:12:01 +0000

    Updated hca2scea input paramaters

--- broker/convert_to_scea/routes.py
+++ broker/convert_to_scea/routes.py
@@ -5,42 +5,27 @@ from http import HTTPStatus
 
 from flask import Blueprint, send_file, request
 from flask_cors import cross_origin
-from hca_to_scea import hca2scea
+from hca2scea import hca2scea
 from ingest.api.ingestapi import IngestApi
 
 from ingest.importer.importer import XlsImporter
 from broker.common.util import response_json
 from broker.convert_to_scea.exceptions import ImportSCEAHttpError, GenerateSCEAFilesError
 
-convert_to_scea_bp = Blueprint(
-    'convert-to-scea', __name__, url_prefix='/'
+get_scea_files_bp = Blueprint(
+    'get-scea-files', __name__, url_prefix='/'
 )
 
 LOGGER = logging.getLogger(__name__)
 
 
-@convert_to_scea_bp.route('/convert_to_scea', methods=['POST'])
+@convert_to_scea_bp.route('/get-scea-files', methods=['POST'])
 @cross_origin(expose_headers=['Content-Disposition'])
 def get_scea_files():
 
-    project_uuid = request.args.get('project_uuid')
-    accession_num = request.args.get('accession_num')
-    curator = request.args.get('curator')
-    experiment_type = request.args.get('experiment_type')
-    factor values = request.args.get('factor values')
-    public_release_date = request.args.get('public_release_date')
-    hca_release_date = request.args.get('hca_release_date')
-    study_accession = request.args.get('study_accession')
+    params_object = request.args.get('params_object')
 
-    output_file = _generate_scea_files(
-    project_uuid,
-    accession_num,
-    curator,
-    experiment_type,
-    factor_values,
-    public_release_date,
-    hca_release_date,
-    study_accession)
+    output_file = _generate_scea_files(params_object)
     filename = f'E-HCAD-{accession_num}.zip'
 
     return _send_file(filename, output_file)
@@ -49,20 +34,22 @@ def get_scea_files():
 def handle_convert_to_scea_http_error(e: ImportSCEAHttpError):
     return response_json(e.status_code, {'message': e.message})
 
-def _generate_scea_files(project_uuid,
-                        accession_num,
-                        curator,
-                        experiment_type,
-                        factor_values,
-                        public_release_date,
-                        hca_release_date,
-                        study_accession):
+def _generate_scea_files(params_object):
 
     try:
-        output_file = hca_to_scea.hca2scea("spreadsheet"=spreadsheet,"project_uuid"=project_uuid,
-        "accession_number"=accession_num,"curators"=curator,"experiment_type"=experiment_type,
-        experimental_factors="factor_values","public_release_date"=public_release_date,
-        "hca_update_date"=hca_release_date,"study"=study_accession)
+        output_file = hca2scea.hca2scea(
+        "spreadsheet"=params_object['spreadsheet'],
+        "project_uuid"=params_object['project_uuid'],
+        "accession_num"=params_object['accession_num'],
+        "curator"=params_object['curator'],
+        "experiment_type"=params_object['experiment_type'],
+        "factor_values'=params_object['factor_values'],
+        "public_release_date"=params_object['public_release_date'],
+        "hca_update_date"=params_object['hca_update_date'],
+        "output_dir"=params_object['output_dir'],
+        "zip_format"=params_boject['zip_format']
+        )
+
     except Exception as e:
         LOGGER.exception(e)
         raise GenerateSCEAFilesError(f'Unable to convert the dataset to SCEA files. [{repr(e)}]')
