de8eb9f8c50801eecf869e99e0237328fd952145
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-10-17 14:10:26 +0100

    dcp-845 disable broken integration test

--- test/integration/test_metadata_spreadsheet_generation.py
+++ test/integration/test_metadata_spreadsheet_generation.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from unittest import TestCase
 
 import polling as polling
@@ -6,26 +7,26 @@ import requests
 from assertpy import assert_that
 
 
-def is_spreadsheet_ready(submission):
-    print('checking submission')
-    return submission['lastSpreadsheetGenerationJob'].get('finishedDate','')
-
-
-
+@unittest.skip
 class TestMetadataSpreadsheetGenerator(TestCase):
+    """
+    I don't think this testing strategy will work, since localhost:5001 isn't up.
+    But we could add this test to the proper integration tests suite
+    """
     def test_spreadsheet_generation(self):
         ingest_url = 'https://api.ingest.dev.archive.data.humancellatlas.org'
         broker_url = 'http://localhost:5001'
 
         submission_uuid = 'c81f7d54-a27f-4212-a6df-88dde947f7cc'
         # generate spreadsheet
-        token = os.environ['INGEST_TOKEN']
+        token = os.getenv('INGEST_TOKEN')
         generate_spreadsheet_response = requests.post(f'{broker_url}/submissions/{submission_uuid}/spreadsheet', headers={'Authorization': token})
-        submission = requests.get(f'{ingest_url}/submissionEnvelopes/search/findByUuidUuid', params={'uuid':submission_uuid}).json()
+        self.assertTrue(generate_spreadsheet_response.ok)
+        submission = requests.get(f'{ingest_url}/submissionEnvelopes/search/findByUuidUuid', params={'uuid': submission_uuid}).json()
         # wait
         polling.poll(
             lambda: requests.get(submission['_links']['self']['href']).json(),
-            check_success=is_spreadsheet_ready,
+            check_success=self.is_spreadsheet_ready,
             step=1,
             timeout=600)
 
@@ -35,12 +36,19 @@ class TestMetadataSpreadsheetGenerator(TestCase):
         # check supplementary files added to project
         self.check_file_in_related_project(submission)
 
-    def check_file_in_related_project(self, submission):
+    @staticmethod
+    def is_spreadsheet_ready(submission) -> bool:
+        print('checking submission')
+        return submission['lastSpreadsheetGenerationJob'].get('finishedDate')
+
+    @staticmethod
+    def check_file_in_related_project(submission):
         project = requests.get(submission['_links']['projcets']['href']).json()[0]
         supplementary_files = requests.get(project['_links']['supplementaryFiles']['href']).json()
         assert_that(supplementary_files).contains({'a': 'y'})
 
-    def check_file_in_submission(self, submission):
+    @staticmethod
+    def check_file_in_submission(submission):
         submission_files = requests.get(submission['_links']['files']['href']).json()
         excels = filter(lambda file: file['content']['file_core']['format'] == 'xlsx', submission_files['files'])
         assert_that(excels).is_not_empty()
