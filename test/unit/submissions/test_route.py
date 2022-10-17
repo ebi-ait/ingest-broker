df11359a64229d76a1be2213ca5ea3db7af7d12a
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-10-17 12:35:34 +0100

    dcp-845 test_route cleanup

--- test/unit/submissions/test_route.py
+++ test/unit/submissions/test_route.py
@@ -14,13 +14,14 @@ class RouteTestCase(BrokerAppTest):
     def setUp(self):
         super().setUp()
         self._app.SPREADSHEET_STORAGE_DIR = 'mock_storage_dir'
-        self.mock_submission = {
+        submission = {
             'lastSpreadsheetGenerationJob': {
                 'finishedDate': '2022-01-27T12:00:58.417Z',
                 'createdDate': '2022-01-27T11:57:05.187Z'
             }
         }
-        self.mock_ingest.get_submission_by_uuid = Mock(return_value=self.mock_submission)
+        self.mock_submission = Mock(return_value=submission)
+        self.mock_ingest.get_submission_by_uuid = self.mock_submission
 
     @patch('broker.submissions.routes.send_file')
     def test_download_spreadsheet__success(self, mock_send_file):
@@ -42,12 +43,9 @@ class RouteTestCase(BrokerAppTest):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         mock_send_file.assert_called_once()
 
-    @patch('broker.submissions.routes.send_file')
-    def test_download_spreadsheet__not_found(self, mock_send_file):
+    def test_download_spreadsheet__not_found(self):
         # given
-        self.mock_submission = {}
-        self.mock_ingest.get_submission_by_uuid = Mock(return_value=self.mock_submission)
-
+        self.mock_submission.return_value = {}
         submission_uuid = 'xyz-001'
 
         with self._app.test_client() as app:
@@ -57,18 +55,16 @@ class RouteTestCase(BrokerAppTest):
         # then
         self.mock_ingest.get_submission_by_uuid.assert_called_once_with(submission_uuid)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-        mock_send_file.assert_not_called()
 
     @patch('broker.submissions.routes.send_file')
     def test_download_spreadsheet__accepted(self, mock_send_file):
         # given
-        self.mock_submission = {
+        self.mock_submission.return_value = {
             'lastSpreadsheetGenerationJob': {
                 'finishedDate': None,
                 'createdDate': '2022-01-27T11:57:05.187Z'
             }
         }
-        self.mock_ingest.get_submission_by_uuid = Mock(return_value=self.mock_submission)
         submission_uuid = 'xyz-001'
 
         with self._app.test_client() as app:
@@ -85,16 +81,10 @@ class RouteTestCase(BrokerAppTest):
     def test_generate_spreadsheet__accepted__finished(self,
                                                       mock_async_export_and_save,
                                                       mock_ingest_api_authenticated):
-        '''
+        """
         tests the happy path for the controller
-        '''
+        """
         # given
-        self.mock_submission = {
-            'lastSpreadsheetGenerationJob': {
-                'createdDate': '2022-01-27T11:57:05.187Z',
-                'finishedDate': '2022-01-27T12:00:58.417Z'
-            }
-        }
         submission_uuid = 'xyz-001'
         with self._app.test_client() as app:
             # when
@@ -104,10 +94,7 @@ class RouteTestCase(BrokerAppTest):
         # then
         self.mock_ingest.get_submission_by_uuid.assert_called_once_with(submission_uuid)
         self.assertEqual(HTTPStatus.ACCEPTED, response.status_code)
-
         mock_async_export_and_save.assert_called_once()
-        # ToDo: Build correct mocking of calls-to and responses-from ingest
-
 
     def test_no_auth_header__bad_request(self):
         submission_uuid = 'xyz-001'
@@ -116,14 +103,13 @@ class RouteTestCase(BrokerAppTest):
             response = app.post(f'/submissions/{submission_uuid}/spreadsheet')
         self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
 
-
     @patch.object(ExportToSpreadsheetService, 'async_export_and_save', return_value='test-job-id')
     @patch('broker.submissions.routes.IngestApi')
     def test_generate_spreadsheet__accepted__not_created(self,
                                                          mock_async_export_and_save,
                                                          mock_ingest_api_authenticated):
         # given
-        self.mock_submission = {
+        self.mock_submission.return_value = {
             'lastSpreadsheetGenerationJob': None
         }
         submission_uuid = 'xyz-001'
@@ -136,7 +122,6 @@ class RouteTestCase(BrokerAppTest):
         # then
         self.mock_ingest.get_submission_by_uuid.assert_called_once_with(submission_uuid)
         self.assertEqual(response.status_code, HTTPStatus.ACCEPTED)
-        # ToDo: Build correct mocking of calls-to and responses-from ingest
 
     @patch.object(ExportToSpreadsheetService, 'async_export_and_save', return_value='test-job-id')
     @patch('broker.submissions.routes.IngestApi')
@@ -144,13 +129,12 @@ class RouteTestCase(BrokerAppTest):
                                                              mock_async_export_and_save,
                                                              mock_ingest_api_authenticated):
         # given
-        self.mock_submission = {
+        self.mock_submission.return_value = {
             'lastSpreadsheetGenerationJob': {
                 'createdDate': '2022-01-27T11:57:05.187Z',
                 'finishedDate': None
             }
         }
-        self.mock_ingest.get_submission_by_uuid = Mock(return_value=self.mock_submission)
         submission_uuid = 'xyz-001'
 
         with self._app.test_client() as app:
